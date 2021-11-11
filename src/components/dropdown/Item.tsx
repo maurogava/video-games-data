b926d17257e037fef59482290f695294b93b2ca4
Mauro Gava <maurogava@gmail.com>
2021-11-11 00:59:04 -0300

    Updates

--- src/components/dropdown/Item.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from 'react'
-
-export interface ItemProps {
-  children: ReactNode
-}
-
-const Item: FC<ItemProps> = ({ children }) => <>{children}</>
-
-export default Item
--- src/components/dropdown/Label.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from 'react'
-
-export interface LabelProps {
-  children: ReactNode
-}
-
-const Label: FC<LabelProps> = ({ children }) => <>{children}</>
-
-export default Label
